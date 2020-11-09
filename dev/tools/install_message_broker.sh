0ae78596e5c534025741dd790ee40cd9ccc62808
Serhii Kovalenko <ganster3012@gmail.com>
2020-11-09 18:08:17 +0200

    Add docker image

--- dev/tools/install_message_broker.sh
+++ dev/tools/install_message_broker.sh
@@ -2,6 +2,5 @@
 echo "Install message broker..."
 bin/magento catalog:message-broker:install --amqp-host=rabbit --amqp-port=5672 --amqp-user=guest --amqp-password=guest
 bin/magento topology:install
-#bin/magento queue:consumers:start catalog.product.export.consumer
-#bin/magento queue:consumers:start catalog.category.export.consumer &
-tail -f /dev/null
+bin/magento queue:consumers:start catalog.product.export.consumer
+bin/magento queue:consumers:start catalog.category.export.consumer &
